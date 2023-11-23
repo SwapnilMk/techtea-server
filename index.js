e50d96383eb850c62f5590e089d83e4bb21af003
SwapnilMk <mswapnil218@gmail.com>
2023-11-23 23:56:37 +0530

    added some backend code !

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+app.use(cors());
 
 app.get("/", (req, res)=>{
-    res.send("<h1>hello world! this is</h1>")
+    res.send("<h1>hello world! this is send method</h1>")
 })
 
 app.listen(port, ()=>{
